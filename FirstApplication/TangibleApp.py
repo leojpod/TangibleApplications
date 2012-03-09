c5dc37ed2724c064b9f6b4c057e227ac09629bcc
Leo <lojeuv-9@student.ltu.se>
2012-03-09 11:30:33 +0100

    now using a socket to receive events from the API

--- FirstApplication/TangibleApp.py
+++ FirstApplication/TangibleApp.py
@@ -1,10 +1,55 @@
 #!/usr/bin/python3
 
-#"""Tangible app is just a simple application that makes a few call to the tangibleAPI and make a couple of request to the devices found"""
+"""Tangible app is just a simple application that makes a few call to the tangibleAPI 
+and make a couple of request to the devices found"""
 import sys
 import httplib2
 import urllib.parse
 import json 
+import asyncore
+import socket
+from threading import Thread
+#static information
+CUBE_NUMBER = 1
+
+
+class EventListener(Thread):
+    def __init__(self, host, port):
+        super(self.__class__,self).__init__()
+        self._connected = False
+        self._dont_stop = True
+        self._host = host
+        self._port = port
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        pass
+    def run(self):
+        try:
+            self._socket.connect( (self._host, self._port) )
+            self._connected = True
+            while(self._dont_stop):
+                self._read()
+            pass
+        except Exception as ex:
+            print("the socket connection failed 'cause of : ",ex)
+            pass
+        pass
+    def _read(self):
+        eventText = ""
+        event_bytes = self._socket.recv(8192)
+        # if (False):
+        #     eventText += event_bytes #event_bytes.decode('utf-8')
+        #     try:
+        #         jsonEvent = json.loads(eventText)
+        #         print('new event: >>> ', json.dumps(jsonEvent, indent = 2))
+        #         pass
+        #     except:
+        #         print('from API body: >>> ', eventText)
+        #         pass
+        #     pass
+        print("we received something: ", repr(event_bytes))
+        pass
+    pass
+pass
 
 def printResponse(resp, content):
     print('from API headers: >>> ',resp)
@@ -64,7 +109,7 @@ sys.stdin.readline()
 print()
 h = httplib2.Http(".cache")
 msg_obj = json.loads(content.decode('utf-8'))
-deviceId = msg_obj['msg'][2]['id']
+deviceId = msg_obj['msg'][CUBE_NUMBER]['id']
 #deviceId = 'myUniqueIdThatIsNotARealOneYet'
 print("requesting the device: ", deviceId)
 #print("json_params : ", json_params)
@@ -92,6 +137,20 @@ while True:
     printResponse(resp, content)
     pass
 
+print("about to make some event testing")
+h = httplib2.Http(".cache")
+resp, content = h.request(baseURI+'/'+appUUID+'/device_methods/'+deviceId+'/subscribe', "PUT")
+printResponse(resp, content)
+print("let's try to connect to the socket")
+msg_obj = json.loads(content.decode('utf-8'))
+port = msg_obj["msg"]["port"]
+port = int(port)
+print("connection information extracted: port=",port)
+sys.stdin.readline()
+listener = EventListener("localhost", port)
+listener.start()
+
+
 sys.stdin.readline()
 print()
 h = httplib2.Http(".cache")
@@ -100,3 +159,5 @@ print("removing the reservation on the device: ", deviceId)
 #print("json_params : ", json_params)
 resp, content = h.request(baseURI+'/'+appUUID+'/device/reservation/'+deviceId, "DELETE")
 printResponse(resp,content)
+
+
